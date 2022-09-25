9e438db1c9cb1773838ea725675eecb2a8096110
yccjs <yccjsalvador@gmail.com>
2022-09-25 11:46:14 +0900

    chore: add code to pass the test, 1 test still failing

--- src/diary/diary_controller.js
+++ src/diary/diary_controller.js
@@ -13,4 +13,21 @@ async function getUserEntries(req, res) {
   return;
 }
 
-module.exports = { getAllEntries, getUserEntries };
+async function addNewEntry(req, res) {
+  const newPost = {
+    username: req.body.username,
+    written_on: req.body.written_on,
+    entry: req.body.entry,
+  };
+  await diaryModel.addNewEntry(newPost);
+  res.status(200).send("successfully added!");
+  return;
+}
+
+async function deleteEntry(req, res) {
+  const id = req.params.id;
+  await diaryModel.deleteEntry(id);
+  res.status(200).send("deleted!");
+  return;
+}
+module.exports = { getAllEntries, getUserEntries, addNewEntry, deleteEntry };
--- src/routes/diary.js
+++ src/routes/diary.js
@@ -9,6 +9,17 @@ const diaryController = require("../diary/diary_controller");
 
 router.get("/entries", diaryController.getAllEntries);
 
+// @desc add new entry
+// @access Public for now
+// @route POST /api/entries
+
+router.post("/entries", diaryController.addNewEntry);
+
+// @desc delete single entry
+// @access Public for now
+// @route DELETE /api/entries/:id
+
+router.delete("/entries", diaryController.addNewEntry);
 // @desc Fetch user entries
 // @access Public for now
 // @route GET /api/myentries?username=
